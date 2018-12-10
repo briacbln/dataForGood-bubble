7d2f7274b1385402b8aade2346a07943894159c2
Briac Belin <briac.belin@gmail.com>
2018-12-10 19:36:32 +0100

    better

--- script.py
+++ script.py
@@ -3,34 +3,29 @@ import sys
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import re
+import urllib2
+import pandas as pd
 
 # def getWords(request):
 #     return request.split()
 
-def getListOfNewspaper():
-    listOfnewspaper = [
-    ['https://www.humanite.fr/search/','%20',-1.0],
-    ['https://www.marianne.net/recherche?k=','+',-0.8],
-    ['https://www.liberation.fr/recherche/?sort=-publication_date_time&period=last_180&period_start_day=0&period_start_month=0&period_start_year=0&period_end_day=0&period_end_month=0&period_end_year=0&editorial_source=&paper_channel=&q=','+',-0.6],
-    ['https://www.lexpress.fr/recherche?q=','+',-0.4],
-    ['https://www.lemonde.fr/recherche/?page_num=1&operator=and&exclude_keywords=&qt=recherche_texte_titre&author=&period=since_1944&start_day=01&start_month=01&start_year=1944&end_day=10&end_month=12&end_year=2018&sort=desc&keywords=','+',-0.2],
-    ['https://www.la-croix.com/Recherche/','%20',0.2],
-    ['https://www.lepoint.fr/recherche/index.php?query=','%20',0.4],
-    ['http://recherche.lefigaro.fr/recherche/','%20',0.6],
-    ['https://www.lejdd.fr/recherche?query=','+',0.8],
-    ['https://recherche.lesechos.fr/recherche.php?exec=1&texte=','+',1.0]
-    ]
-    return listOfnewspaper
+def getListOfNewspaper(file):
+    df = pd.read_csv(file,header=0,names=['index','racine','racine_recherche','addword','scale'])
+    df['scale'] = df['scale'].convert_objects(convert_numeric=True)
+    df.set_index('index')
+    return df
     # 'liberation','lenouvelobs','lexpress','lemonde','lacroix',
     # 'lepoint','lefigaro','jdd','echos','tribune']
 
 def printList(list):
     for e in list:
-        print e
+        print(e)
 
 def takeRandomElements(list):
     number_of_articles = 3
-    randomElements = random.sample(list,number_of_articles)
+    randomElements = list.sample(n=number_of_articles)
     if debug :
         print('Random Elements')
         print(randomElements)
@@ -42,9 +37,11 @@ def getBestSort(possibilities):
     sqrdList = []
     tempList = []
     for possibility in possibilities :
-        avg = sum(newspaper[2] for newspaper in possibility)/float(nb)
+        for newspaper in possibility :
+            print (newspaper,newspaper[3],type(newspaper[3]))
+        avg = sum(newspaper[4] for newspaper in possibility)/float(nb)
         avgList.append(avg)
-        sqrd = sum(newspaper[2]**2 for newspaper in possibility)/float(nb)
+        sqrd = sum(newspaper[4]**2 for newspaper in possibility)/float(nb)
         sqrdList.append(sqrd)
         temp = sqrd/(0.00001+avg)
         tempList.append(temp)
@@ -64,37 +61,62 @@ def getBestSort(possibilities):
         print(avgList[bestchoice],sqrdList[bestchoice],tempList[bestchoice])
     return bestchoice
 
-def doMultipleSelections():
+def doMultipleSelections(db):
     number_of_selections = 50
     possibilities = []
-    db = getListOfNewspaper()
     for i in range(number_of_selections):
-        possibilities.append(takeRandomElements(db))
+        possibilities.append(takeRandomElements(db).values)
     bestchoice = getBestSort(possibilities)
     if debug :
         print('Choix final')
         printList(possibilities[bestchoice])
     return possibilities[bestchoice]
 
-
 def getListofURL(requestSplitted,list):
     listOfUrl = []
     for newsPaper in list :
-        url = newsPaper[0]
+        url = newsPaper[2]
         for word in requestSplitted :
-            url+=str(word)+newsPaper[1]
-        listOfUrl.append(url)
+            url+=str(word)+newsPaper[3]
+        listOfUrl.append([newsPaper[0],url])
     printList(listOfUrl)
     return listOfUrl
 
-if __name__ == "__main__":
-    # SEARCH = "gilets jaunes"
+def getArticles(listofpages,db):
+    listofarticles = []
+    for index,page in listofpages :
+        # r = requests.get(page)
+        # print(r.text)
+        # r2 = rh.HTML(html=page)
+        # print(r2.links)
+        html_page = urllib2.urlopen(page)
+        soup = BeautifulSoup(html_page,'lxml')
+        if debug:
+            print(db.loc[db['index']==index]['racine'].values[0])
+        for link in soup.findAll('a'):#, attrs={'href': re.compile('^'+db.loc[db['index']==index]['racine'].values[0])}):
+            print(link.get('href'))
+        # break
+    return listofarticles
+
+def test(nb):
     debug = False
-    SEARCH = sys.argv[1:]
     T = []
-    for i in range(10000):
+    for i in range(nb):
         listofnewspaper = doMultipleSelections()
-        listofurl = getListofURL(SEARCH,listofnewspaper)
+        listofurl = getListofURL('test',listofnewspaper)
         T+=listofurl
     plt.hist(T)
     plt.show()
+
+
+
+
+if __name__ == "__main__":
+    # SEARCH = "gilets jaunes"
+    debug = True
+    SEARCH = sys.argv[1:]
+    file = 'db.csv'
+    db = getListOfNewspaper(file)
+    listofnewspaper = doMultipleSelections(db)
+    listofsearchurl = getListofURL(SEARCH,listofnewspaper)
+    listofarticles = getArticles(listofsearchurl,db)
